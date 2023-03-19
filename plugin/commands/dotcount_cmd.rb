49a9caeeeaf14a46c03f68917eb3e3e20080900d
Ren <59793669+SerendipityJones@users.noreply.github.com>
2023-03-19 14:40:21 -0700

    Fix locales, still poking refactor

--- plugin/commands/dotcount_cmd.rb
+++ plugin/commands/dotcount_cmd.rb
@@ -10,10 +10,10 @@ module AresMUSH
       end
  
       def handle
-        Dotcount.calculate_dots(self.name, client, enactor)
-        pad_xp = (result[max_xp] > 9 && result[current_xp] < 10) ? '.' : ''
-        note = result[poor_attr] ? t('dotcount.poor_note_client') : ''
-        msg = t('dotcount.result_client', :name => self.name, :spent_attrs => result[spent_attrs], :spent_action => result[spent_action], :max_attrs => result[max_attrs], :max_action => result[max_action], :max_xp => result[max_xp], :current_xp => result[current_xp].to_s, :pad_xp => pad_xp, :note => note )
+        result = Dotcount.calculate_dots(self.name, client, enactor)
+        pad_xp = (result["max_xp"] > 9 && result["current_xp"] < 10) ? '.' : ''
+        note = result["poor_attr"] ? t('dotcount.poor_note_client') : ''
+        msg = t('dotcount.result_client', :name => self.name, :spent_attrs => result["spent_attrs"], :spent_action => result["spent_action"], :max_attrs => result["max_attrs"], :max_action => result["max_action"], :max_xp => result["max_xp"], :current_xp => result["current_xp"].to_s, :pad_xp => pad_xp, :note => note )
         template = BorderedDisplayTemplate.new msg
         client.emit template.render
      end
