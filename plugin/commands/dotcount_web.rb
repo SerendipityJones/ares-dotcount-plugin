48466af6199bb97a835d0a949317915f568e7c8e
Ren <59793669+SerendipityJones@users.noreply.github.com>
2023-03-19 13:28:53 -0700

    Update dotcount_web.rb

--- plugin/commands/dotcount_web.rb
+++ plugin/commands/dotcount_web.rb
@@ -2,23 +2,22 @@ module AresMUSH
     module Dotcount  
       def self.web_dotcount(char)
         Dotcount.calculate_dots(char, Character, char)
-          if remaining_attrs == 0 && remaining_action == 0
-            msg = t('dotcount.web_max_both')
-          elsif remaining_attrs == 0
-            msg = t('dotcount.web_max_attrs', :remaining_action => remaining_action, :plural => remaining_action == 1 ? '' : 's' )
+        if remaining_attrs == 0 && remaining_action == 0
+          msg = t('dotcount.web_max_both')
+        elsif remaining_attrs == 0
+          msg = t('dotcount.web_max_attrs', :remaining_action => remaining_action, :plural => remaining_action == 1 ? '' : 's' )
+        else
+          msg =t('dotcount.web_more_attrs',:remaining_attrs => remaining_attrs, :plural => remaining_attrs == 1 ? '' : 's' )
+          if remaining_action == 0
+            msg += t('dotcount.web_max_action')
           else
-            msg =t('dotcount.web_more_attrs',:remaining_attrs => remaining_attrs, :plural => remaining_attrs == 1 ? '' : 's' )
-            if remaining_action == 0
-              msg += t('dotcount.web_max_action')
-            else
-              msg += t('dotcount.web_more_action', :remaining_action => remaining_action)
-            end
+            msg += t('dotcount.web_more_action', :remaining_action => remaining_action)
           end
-          if poor_attr
-            msg += t('dotcount.poor_note_web')
-          end
-          return msg
         end
+        if poor_attr
+          msg += t('dotcount.poor_note_web')
+        end
+        return msg
       end
     end
   end
